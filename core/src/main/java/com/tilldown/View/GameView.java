521f783cce49a0d843b33be75120faa48e3acb11
Hosein_withg <hosein.bakhoda2006@gmail.com>
2025-05-22 01:31:42 +0330

    special cursor

--- core/src/main/java/com/tilldown/View/GameView.java
+++ core/src/main/java/com/tilldown/View/GameView.java
@@ -3,20 +3,28 @@ package com.tilldown.View;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.tilldown.Controller.GameController;
 import com.tilldown.Main;
 
+
 public class GameView implements Screen, InputProcessor {
     private Stage stage;
+    private Texture cursorTexture;
+    private Sprite cursorSprite;
     private GameController controller;
 
     public GameView(GameController controller) {
         this.controller = controller;
         controller.setView(this);
+        //Gdx.input.setCursorCatched(true);
+        Gdx.input.setCursorPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        cursorTexture = new Texture("cursor.png");
+        cursorSprite = new Sprite(cursorTexture);
     }
 
     @Override
@@ -28,12 +36,13 @@ public class GameView implements Screen, InputProcessor {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
+        cursorSprite.setPosition(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
         Main.getBatch().begin();
         controller.updateGame();
+        //cursorSprite.draw(Main.getBatch());
         Main.getBatch().end();
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
-
     }
 
     @Override
@@ -58,6 +67,8 @@ public class GameView implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
+        cursorTexture.dispose();
+        stage.dispose();
     }
 
     @Override
